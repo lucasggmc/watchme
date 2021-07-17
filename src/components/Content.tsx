9119b73b5f9daeb30113c29b9f461504069de3db
lucasggmc <lucasggmc@gmail.com>
2021-07-17 16:44:55 -0300

    finalizado componentização da aplicação desafio-complementar/modulo01

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,50 @@
-export function Content() {
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+import '../styles/content.scss';
+
+interface ContentProps {
+  genreId: number;
+  genreTitle: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content({genreId, genreTitle} : ContentProps) {
   // Complete aqui
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${genreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [genreId])
+
+  return(
+    <div className="container">
+        <header>          
+          <span className="category">Categoria:<span> {genreTitle}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
+
 }
